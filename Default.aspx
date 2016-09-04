dd4b8708b44089f8a2d23614c9a0fae30eefa222
AurelijaZ <aurelijazuba@gmail.com>
2016-09-04 22:25:34 +0100

    ...

---
+++ Default.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hangman Game</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Hangman</h1>
+
+        
+                
+        <br />
+        <br />
+
+        <asp:Label ID="WordResult" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="TestWord" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="Test2" runat="server" Text=""></asp:Label>
+        <br />
+        <br />
+
+        <asp:Button ID="Button1" runat="server" Text="A" />
+        <asp:Button ID="Button2" runat="server" Text="B" />
+        <asp:Button ID="Button3" runat="server" Text="C" />
+        <asp:Button ID="Button4" runat="server" Text="D" />
+        <asp:Button ID="Button5" runat="server" Text="E" />
+        <asp:Button ID="Button6" runat="server" Text="F" />
+        <asp:Button ID="Button7" runat="server" Text="G" />
+        <asp:Button ID="Button8" runat="server" Text="H" />
+        <asp:Button ID="Button9" runat="server" Text="I" />
+        <asp:Button ID="Button10" runat="server" Text="J" />
+        <asp:Button ID="Button11" runat="server" Text="K" />
+        <asp:Button ID="Button12" runat="server" Text="L" />
+        <asp:Button ID="Button13" runat="server" Text="M" />
+        <br />
+        <br />
+        <asp:Button ID="Button14" runat="server" Text="N" />
+        <asp:Button ID="Button15" runat="server" Text="O" />
+        <asp:Button ID="Button16" runat="server" Text="P" />
+        <asp:Button ID="Button17" runat="server" Text="Q" />
+        <asp:Button ID="Button18" runat="server" Text="R" />
+        <asp:Button ID="Button19" runat="server" Text="S" />
+        <asp:Button ID="Button20" runat="server" Text="T" />
+        <asp:Button ID="Button21" runat="server" Text="U" />
+        <asp:Button ID="Button22" runat="server" Text="V" />
+        <asp:Button ID="Button23" runat="server" Text="W" />
+        <asp:Button ID="Button24" runat="server" Text="X" />
+        <asp:Button ID="Button25" runat="server" Text="Y" />
+        <asp:Button ID="Button26" runat="server" Text="Z" />
+        
+        <asp:Button ID="ButtonWord" runat="server" Text="New Word" />
+        <br />
+        <br />
+        <asp:Button ID="Buttonexit" runat="server" Text="Exit" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    //add all properties of images together
+    private Bitmap [] hangImages = {   };
+    //assing private int to wring guesses
+    private int wrongGuesses = 0;
+    //private int rightGuesses = 0;
+    private string current = "";
+    private string copyCurrent = "";
+    private string[] words;
+    //private string lblword;
+
+
+
+
+
+    protected void ButtonWord_Click (object sender, EventArgs e)
+    {
+        SetupWordChoice();
+        Test2.Text = "";
+        //Enable Alphabet buttons with the press of New Button
+        foreach (Control control in Controls)
+        {
+            Button letter = control as Button;
+            if (letter != null)
+            {
+                letter.Enabled = true;
+            }
+
+        }
+    }
+
+
+    protected void loadwords()
+    {
+        char[] delimiterChars = { ',' };
+        string[] readText = System.IO.File.ReadAllLines(@"C:\Users\PC\Desktop\Hangman Project\words.txt");
+        words = new string[readText.Length];
+        int index = 0;
+        foreach (string s in readText)
+        {
+            string[] line = s.Split(delimiterChars);
+            words[index++] = line[0];
+        }
+
+    }
+
+    protected void SetupWordChoice()
+    {
+        //making a guess
+        wrongGuesses = 0;
+        // hangImage1.Image = hangImages[wrongGuesses];
+        int guessIndex = (new Random()).Next(words.Length); //generates random number for the words 
+        current = words[guessIndex];
+
+        TestWord.Text = current;
+
+        //make a copy of a guess
+        copyCurrent = "";
+        for (int index = 0; index < current.Length; index++)
+        {
+            copyCurrent += "_";
+        }
+        displayCopy();
+
+    }
+    //display to the label above function
+    protected void displayCopy()
+    {
+        WordResult.Text = "";
+
+        for (int index = 0; index < copyCurrent.Length; index++)
+        {
+            WordResult.Text += copyCurrent.Substring(index, 1);
+            WordResult.Text += " ";
+            //return lblword.Text(words[Math.Floor(Math.random() * words.Length)]);
+
+
+        }
+    }
+
+    private void updateCopy(char guess)
+    {
+
+    }
+
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Button choice = sender as Button;
+        choice.Enabled = false;
+
+        if (current.Contains(choice.Text))
+        {
+            char[] temp = copyCurrent.ToCharArray();
+            char[] find = current.ToCharArray();
+            char guessChar = choice.Text.ElementAt(0);
+
+            for (int index = 0; index < find.Length; index++)
+            {
+                if (find[index] == guessChar)
+                {
+                    temp[index] = guessChar;
+                }
+                copyCurrent = new string(temp);
+
+
+                //change it up to find and display the found letter otherwise 
+
+            }
+            displayCopy();
+        }
+    
+
+
+        else
+        {
+            wrongGuesses++;
+        }
+
+
+        if (wrongGuesses <= 7)
+        {
+           // hangImage1.Image = hangImages[wrongGuesses];
+        }
+
+        else
+        {
+            Test2.Text = "You Lost!";
+        }
+        if (copyCurrent.Equals(current))
+        {
+            Test2.Text = "Yay, you won!!!";
+        }
+
+        TestWord.Text = choice.Text;
+    }
+
+    //Form application
+    protected void form1_Load(object sender, EventArgs e)
+    {
+        loadwords();
+        SetupWordChoice();
+    }
+
+    //Exit button
+    protected void Buttonexit_Click (object sender, EventArgs e)
+    {
+         //Application.Exit();
+    }
+
+}
+
+
